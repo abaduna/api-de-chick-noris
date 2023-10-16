657ce1c6f8111052d99c60915478b1fed5104f6d
abaduna <64230830+abaduna@users.noreply.github.com>
2023-10-15 22:52:04 -0300

    terminado el css

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const  llamada_A_la_API=async()=>{
     let url ="https://api.chucknorris.io/jokes/random"
     const api = await fetch(url)
     const  data = await api.json()
-    console.log(data)
+
     escrivirData(data)
 }
 const escrivirData = (data)=>{
@@ -16,4 +16,27 @@ const escrivirData = (data)=>{
   </div>`
 
 }
+const  llamada_de_categoria=async(catogoria ="animal")=>{
+    let url =`https://api.chucknorris.io/jokes/random?category=${catogoria}`
+    console.log(`el ur es ${url}`)
+    // let url =`https://api.chucknorris.io/jokes/random?category=animal`
+    const api = await fetch(url)
+    const  data = await api.json()
+    console.log(data)
+    escrivirDataCatogoria(data)
+
+}
+const escrivirDataCatogoria = (data)=>{
+
+    divRes = document.querySelector("#categoria")
+    divRes.innerHTML = ` <div class="card " style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${data.value}</h5>
+      <p class="card-text"> la cateogria es ${data.categories}</p>
+      
+    </div>
+  </div>`
+
+}
 llamada_A_la_API()
+llamada_de_categoria()
